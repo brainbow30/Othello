528755374560b183dd7fd07d9b4229a3bfe12bf3
brain <brainbow30@gmail.com>
2020-01-13 12:14:02 +0000

    changes to mockito and mocked objects

--- src/test/java/application/mcts/GenerateNNDataTest.java
+++ src/test/java/application/mcts/GenerateNNDataTest.java
@@ -4,24 +4,20 @@ import application.game.Board;
 import application.game.COLOUR;
 import application.game.verifiers.OthelloVerifier;
 import com.google.common.collect.ImmutableList;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class GenerateNNDataTest {
     private Board board;
     private TreeNode treeNode;
 
-    private GenerateNNData generateNNData;
-    @Before
+    private GenerateNNData GenerateNNData;
+
     public void setup() {
-        generateNNData = new GenerateNNData("testWrite.txt");
+        GenerateNNData = new GenerateNNData("testWrite.txt");
         board = new Board(4, new OthelloVerifier(), 0.0, 0.0, 0.0);
         treeNode = TreeNode.builder().colour(COLOUR.WHITE).currentBoard(board).positionToCreateBoard(null).parent(null).rootColour(COLOUR.WHITE).hostname("127.0.0.1:5000").build();
     }
@@ -32,7 +28,7 @@ public class GenerateNNDataTest {
         try {
             ImmutableList.Builder<Double> builder = ImmutableList.builder();
             builder.add(0.0);
-            generateNNData.write(board.asIntArray(), builder.build(), 1);
+            GenerateNNData.write(board.asIntArray(), builder.build(), 1);
             String expected = "[[0,0,0,0,0,1,-1,0,0,-1,1,0,0,0,0,0],[0.0],1]";
             assertEquals(expected, readFile().get(0));
         } catch (FileNotFoundException e) {
@@ -42,11 +38,10 @@ public class GenerateNNDataTest {
         }
     }
 
-    @Test
     public void save() {
-        generateNNData.open();
-        generateNNData.save(treeNode);
-        generateNNData.close();
+        GenerateNNData.open();
+        GenerateNNData.save(treeNode);
+        GenerateNNData.close();
     }
 
     private ImmutableList<String> readFile() throws IOException {
