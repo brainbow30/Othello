9996e6b73fafafb46ac522494c7d5c8844c0d07d
brain <brainbow30@gmail.com>
2020-03-24 14:17:23 +0000

    black predominance

--- src/main/java/application/mcts/GenerateNNData.java
+++ src/main/java/application/mcts/GenerateNNData.java
@@ -8,7 +8,7 @@ public class GenerateNNData {
 
     static ImmutableList<Integer> canonicalBoard(TreeNode node) {
         ImmutableList<Integer> intBoard = node.getCurrentBoard().asIntArray();
-        if (node.getRootColour().equals(COLOUR.BLACK)) {
+        if (node.getRootColour().equals(COLOUR.WHITE)) {
             return changeBoardPerspective(intBoard, node.getCurrentBoard().getBoardSize());
         }
         return intBoard;
